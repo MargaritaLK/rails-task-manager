8a179cf85e8e24a157d9811633acc81682d860bf
Margarita <l.m.klingen@gmail.com>
2019-08-13 15:45:06 +0200

    wrking task app, still needs styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  # nieuw task maken in form
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # show details of one
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  post 'tasks', to: 'tasks#create', as: 'created_task'
+
+  # updating
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update'
+
+  # destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
